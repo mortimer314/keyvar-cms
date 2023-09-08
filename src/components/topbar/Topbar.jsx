b22511b2cf3686439ad11eab61f71cbc664726fe
morteza <hadafmahdi@gmail.com>
2023-09-08 23:33:09 +0330

    add base topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,7 +4,24 @@ export default function Topbar() {
   return (
     <div className='topbar'>
         <div className="topbarWrapper">
-            topbar 
+            <div className="topbar__left">
+                <div className="topbar__logo">
+                    <img src="./images/logo.png" alt="logo" />
+                </div>
+                <div className="topbar__brand">
+                    <h1>Keyvar</h1>
+                </div>
+            </div>
+            <div className="topbar__center">
+                <div className="topbar__search">
+                    <div className="topbar__search-icon"></div>
+                    <input type="text" />
+                    <div className="topbar__search-close-icon">
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="topbar__right">x</div>
         </div>
     </div>
   )
